4a1a92ae0e827dcddcabed889eba4d8107babd98
Pedro San Roman <pedroceferino.sanroman@gmail.com>
2025-03-13 11:50:36 +0100

    Ejercicio resuelto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+
+const agregar = document.getElementById ('agregar');
+const lista = document.getElementById ('lista')
+const resultado = function () {
+    const texto = prompt ('Introduce aquí lo que quieras añadir');
+    if (texto) {
+        lista.innerHTML += '<li>' + texto + '</li>'
+    }
+    }
+
+agregar.addEventListener ('click', resultado)
